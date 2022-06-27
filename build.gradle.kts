1e99d3482ec5010233e81c36ab86780038c83546
Matthmul <73168857+Matthmul@users.noreply.github.com>
2022-06-28 01:05:48 +0200

    Added Ktor Bot

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+val slack_version: String by project
+
+plugins {
+    kotlin("plugin.serialization") version "1.6.21"
+    kotlin("jvm") version "1.6.21"
+    application
+}
+
+group = "org.bot"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("com.slack.api:slack-api-client:$slack_version")
+    implementation("io.ktor:ktor-client-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
---
+++ src/main/kotlin/bot/Routing.kt
@@ -0,0 +1,98 @@
+package bot
+
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import kotlinx.serialization.json.Json
+
+fun returnCategories(): String {
+    val categories = mutableMapOf<Int, String>()
+    categories.put(1, "Zupki")
+    categories.put(2, "Ketchupy")
+    categories.put(3, "Chipsy")
+
+    var response = ""
+
+    categories.forEach { category ->
+        response += "${category.key}: ${category.value}\n"
+    }
+
+    return  response
+}
+
+fun returnCategoriesItmes(categoryId: String): String {
+    val zupki = mutableMapOf<Int, String>()
+    zupki.put(1, "Zupka chińska 10zł")
+    zupki.put(2, "Zupka japońska 15zł")
+
+    val ketchupy = mutableMapOf<Int, String>()
+    ketchupy.put(1, "Heinz 15zł")
+    ketchupy.put(2, "Tortex 18zł")
+    ketchupy.put(3, "Pudliszki 4zł")
+
+    val chips = mutableMapOf<Int, String>()
+    chips.put(1, "Lay's 25zł")
+    chips.put(2, "Cheetos 20zł")
+    chips.put(3, "Crunchips 30zł")
+    chips.put(3, "Pringles 40zł")
+
+    var response = ""
+
+    when (categoryId.toInt()) {
+        1 -> {
+            zupki.forEach { category ->
+                response += "${category.key}: ${category.value}\n"
+            }
+
+            return response
+        }
+        2 -> {
+            ketchupy.forEach { category ->
+                response += "${category.key}: ${category.value}\n"
+            }
+            return response
+        }
+        3 -> {
+            chips.forEach { category ->
+                response += "${category.key}: ${category.value}\n"
+            }
+            return response
+        }
+        else -> {
+            return "Nieprawidłowa kategoria"
+        }
+    }
+}
+
+fun Application.configureRouting() {
+    install(ContentNegotiation) {
+        json(
+            Json {
+                ignoreUnknownKeys = true
+            }
+        )
+    }
+
+    routing {
+        post("/hello") {
+            call.respondText("Witaj w jestem botem!")
+        }
+
+        post("/category") {
+            call.respondText(returnCategories())
+        }
+
+        post("/products") {
+            val formParameters = call.receiveParameters()
+            var id = formParameters["text"]
+            if (id == null) {
+                id = "-420"
+            }
+
+            call.respondText(returnCategoriesItmes(id))
+        }
+    }
+}
